854a45ffd56a95185796f175bd1e98241c8611ff
pxl9588 <pxl9588@g.rit.edu>
2019-04-23 16:40:28 -0400

    I think it should work now

--- serialRead.py
+++ serialRead.py
@@ -5,6 +5,8 @@ import time
 import threading
 import queue
 
+debug = 1
+
 led = gpiozero.LED("BOARD7")
 ser = serial.Serial("/dev/ttyAMA0", baudrate=19200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=3.0)
 
@@ -14,8 +16,9 @@ distanceToRadar = .0005
 
 def carPassed(window):
 	#For debugging only
-	window.addstr(2, 0, "Car passed")
-	window.clrtoeol()
+        if debug:
+                window.addstr(2, 0, "Car passed")
+                window.clrtoeol()
 
 	#Turns the LED on to stop transmitting the warning
 	led.on()
@@ -39,29 +42,35 @@ def main(window):
 			timeToIntersection = (distanceToRadar/float(speed))*3600
 
 			#If new time to intersection is longer update time to intersection
-			if timeToIntersection > oldTimeToIntersection:
+			if timeToIntersection > oldTimeToIntersection - deltaTime:
 				oldTimeToIntersection = timeToIntersection
 
 				#For debugging only
-				window.addstr(0, 0, "Car will be at intersection in : {0} seconds".format(timeToIntersection))
-				window.clrtoeol()
+				if debug:
+                                        window.addstr(0, 0, "Car will be at intersection in : {0} seconds".format(timeToIntersection))
+                                        window.clrtoeol()
 
 				#Start the timer
 				startTime = time.clock()
+
+				#Turn led off to transmit 1
+				led.off()
 
 			#Calculate time since timer started
 			deltaTime = time.clock() - startTime
 
 			#For debugging only
-			window.addstr(1, 0, "timePassed: {0} seconds".format(deltaTime))
-			window.clrtoeol()
+			if debug:
+                                window.addstr(1, 0, "timePassed: {0} seconds".format(deltaTime))
+                                window.clrtoeol()
 
 			#Check if the time passed exceeds the expected time
 			if deltaTime >= oldTimeToIntersection:
 				carPassed(window)
 
 			#For debugging only
-			window.refresh()
+			if debug:
+                                window.refresh()
 
 
 curses.wrapper(main)
